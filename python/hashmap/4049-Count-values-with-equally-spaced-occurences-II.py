fed5d79c1903a94d3ed2cee40a79819a41c1f6ff
Abhishek Choudhary <choudharyabhishek9876@gmail.com>
2026-09-15 11:33:23 +0300

    Leetcode 4049-Count values with equally spaced occurrences II

--- python/hashmap/4049-Count-values-with-equally-spaced-occurences-II.py
+++ python/hashmap/4049-Count-values-with-equally-spaced-occurences-II.py
@@ -1,11 +1,13 @@
 # Problem: Leetcode 4049 - Count values with equally spaced occurences I
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/count-values-with-equally-space-occurrences-i
-# .py/description/
+# Link: https://leetcode.com/problems/count-values-with-equally-space-occurrences-ii/description/
 # Time Complexity: O(n)
-# Space Complexity: O(n) as we use a hashmap
-# Approach: We just make hashmap of frequencies and then we iterate over the values and see if it occurs thrice by checking length of its value list and 
-# if it does then we check distance
+# Space Complexity: O(n) as we two hashmaps and a separate one to keep check on distances to avoid nested loops as that wont work
+# Approach: We just make hashmap of frequencies and then we iterate over the values and see if it occurs thrice by checking length of its value list.
+# while collecting the values we also use a hashmap of set to keep adding the distance between current index found and the previous index.
+# the set will remove duplicated so that when we finally check if element occurs over 3 times, then if the len of that element value in the t hashmap is 1 that
+# means only one distance was found between all indices which means they are equidistant and then we can increment our count variable.
+# we return count at the end.
 
 from collections import defaultdict
 class Solution:
